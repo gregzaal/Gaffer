6e8ca7cd031918f48b0c6a4519995447a150d5dc
Greg <gregzzmail@gmail.com>
2016-12-22 11:28:36 +0200

    Set min version
    As suggested by Sir @TheDuckCow - otherwise users can't downgrade to older versions since __init__.py didn't exist before then.

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -765,7 +765,7 @@ def register(bl_info):
 	# to skip showing for udpater; see code for function above.
 	# Set the min and max versions allowed to install.
 	# Optional, default None
-	updater.version_min_update = (0,0,0) # min install (>=) will install this and higher
+	updater.version_min_update = (2,6,0) # min install (>=) will install this and higher
 	# updater.version_min_update = None  # if not wanting to define a min
 	updater.version_max_update = (9,9,9) # max install (<) will install strictly anything lower
 	# updater.version_max_update = None  # if not wanting to define a max
