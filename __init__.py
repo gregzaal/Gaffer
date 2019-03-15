84de7a441cb3ad0d100746fc93697ed609b2b4b9
Greg <gregzzmail@gmail.com>
2019-03-15 14:41:22 +0200

    Whoops :)

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ bl_info = {
     "name": "Gaffer",
     "description": "Master your lighting workflow with easy access to lamp properties and other tools",
     "author": "Greg Zaal",
-    "version": (3, 0, 6),
+    "version": (3, 0, 5),
     "blender": (2, 79, 5),
     "location": "3D View > Tools  &  World Settings > HDRI",
     "warning": "",
