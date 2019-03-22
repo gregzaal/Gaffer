dfc6506503b61f6f4a25d33ffe4220ccab79bd72
Greg <gregzzmail@gmail.com>
2019-03-22 12:55:53 +0200

    Remove min version update in case user needs to downgrade for b2.7

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -1397,7 +1397,7 @@ def register(bl_info):
 	# Set the min and max versions allowed to install.
 	# Optional, default None
 	# min install (>=) will install this and higher
-	updater.version_min_update = (3,1,0)
+	updater.version_min_update = None
 	# updater.version_min_update = None  # if not wanting to define a min
 
 	# max install (<) will install strictly anything lower
